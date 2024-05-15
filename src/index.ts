4bb4a9587e4d801ca90c9617cbabcc951bdf41a3
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2024-05-15 15:49:53 -0400

    Make express hello world.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 5432;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
