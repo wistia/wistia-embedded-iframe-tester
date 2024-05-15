70965102acae50c9c5a83018b1cb6252787742e8
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2024-05-15 16:13:51 -0400

    Make hashed id token come from URL.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const searchParams = new URL(document.location.toString()).searchParams
+  const hashedId = searchParams.get('hashedId') ?? ''
+
   return (
     <div>
       <h2>Look at my transcript</h2>
-      <iframe title="embed" src="https://embed.wistia.io/transcript-edit/embed/?hashedId=m8pda3tl3c&token=expiring_token_384f92e50a1e264caa985e9e6f7b595a6897fd9d493dd41f916cfafd758673ff" />
+      <iframe title="embed" src={`https://embed.wistia.io/transcript-edit/embed/?hashedId=${hashedId}&token=expiring_token_384f92e50a1e264caa985e9e6f7b595a6897fd9d493dd41f916cfafd758673ff`} />
     </div>
   );
 }
