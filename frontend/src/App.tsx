c8f5a49eac063391dc445291ff7cc9a993f96f5f
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2024-05-15 16:07:40 -0400

    Hard code hashed id and token in frontend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Look at my transcript</h2>
+      <iframe title="embed" src="https://embed.wistia.io/transcript-edit/embed/?hashedId=m8pda3tl3c&token=expiring_token_384f92e50a1e264caa985e9e6f7b595a6897fd9d493dd41f916cfafd758673ff" />
     </div>
   );
 }
