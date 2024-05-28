4b28de275ef6f2f6413485623f346b6262069c4c
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2024-05-28 11:34:51 -0400

    Make it so the token gets passed by postMessage instead of the url.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   const searchParams = new URL(document.location.toString()).searchParams
   const hashedId = searchParams.get('hashedId') ?? ''
   const [token, setToken] = useState<string | undefined>()
+  const [iframeRendered, setRendered] = useState<boolean>()
 
   useEffect(() => {
     const controller = new AbortController()
@@ -26,14 +27,32 @@ function App() {
     return () => controller.abort();
   }, [hashedId, token])
 
-  if (token === undefined) {
-    return null;
-  }
+  useEffect(() => {
+    const iframeMessageListener = (event: MessageEvent) => {
+      const { data } = event as { data: { type?: string; value?: boolean } };
+
+      if (data.type === 'rendered') {
+        setRendered(!!data.value);
+      }
+    };
+
+    window.addEventListener('message', iframeMessageListener);
+
+    return () => {
+      window.removeEventListener('message', iframeMessageListener);
+    };
+  }, [])
+
+  useEffect(() => {
+    if (iframeRendered) {
+      document.querySelector('iframe')?.contentWindow?.postMessage({type: 'token', value: token}, '*')
+    }
+  }, [token, iframeRendered]);
 
   return (
     <div>
       <h2>Look at my transcript</h2>
-      <iframe title="embed" src={`https://embed.wistia.io/transcript-edit/embed/?hashedId=${hashedId}&token=${token}`} sandbox="allow-scripts allow-same-origin allow-modals" />
+      <iframe title="embed" src={`https://embed.wistia.io/transcript-edit/embed/?hashedId=${hashedId}`} sandbox="allow-scripts allow-same-origin allow-modals" />
     </div>
   );
 }
