9e22cab184d806eeb5e1a8ff015ed5931b0266ca
Thijs de Vries <48684+thijsnado@users.noreply.github.com>
2024-07-03 13:50:46 -0400

    Improve showing/hiding experience.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -87,13 +87,20 @@ function App() {
 
     if (toggledValue || !iframeBeingEdited || window.confirm("Iframe is being editted, are you sure you want to continue?")) {
       setShowIframe(toggledValue);
+
+      // We cannot get the final disconnect messages so we need to manually reset
+      // values we get from the iframe when we hide it.
+      if (!toggledValue) {
+        setRendered(false);
+        setIframeBeingEdited(false);
+      }
     }
   }
 
   return (
     <div>
       <h1>React in TypeScript embed example</h1>
-      <button type="button" onClick={handleSetShowIframe}>Click me!</button><br /><br />
+      <button type="button" onClick={handleSetShowIframe}>{showIframe ? 'Hide editor' : 'Show editor'}</button><br /><br />
       {showIframe && <iframe title="embed" src={url} sandbox="allow-scripts allow-same-origin allow-modals" />}
     </div>
   );
